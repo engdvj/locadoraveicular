49a67c9c3b4742b503cf4741011a81bdfc240766
engdvj <eng.dvj@gmail.com>
2024-03-10 22:12:40 -0300

    CommandLine App

---
+++ src/br/com/projetos/locadoraveiculos/view/commandLine/App.java
@@ -0,0 +1,56 @@
+package br.com.projetos.locadoraveiculos.view.commandLine;
+
+import br.com.projetos.locadoraveiculos.controller.locadora.*;
+import br.com.projetos.locadoraveiculos.controller.sistemas.*;
+import br.com.projetos.locadoraveiculos.model.clientes.*;
+import br.com.projetos.locadoraveiculos.model.veiculo.*;
+import br.com.projetos.locadoraveiculos.util.Util;
+import br.com.projetos.locadoraveiculos.view.menu.MenuInicial;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+
+public class App {
+    private final ControllerLocadora controller;
+    private final MenuInicial menuInicial;
+    public static final Scanner scanner = new Scanner(System.in);
+
+
+    public App(ControllerLocadora controllerLocadora) {
+        this.controller = controllerLocadora;
+        this.menuInicial = new MenuInicial(controllerLocadora);
+    }
+
+    public void run(){
+        menuInicial.escolherOpcao();
+    }
+    public static List<Veiculo> loadSampleVeiculos(){
+        List<Veiculo> veiculos = new ArrayList<>();
+        veiculos.add(new Veiculo("Ford", "Focus", "123456789", Util.Tamanho.MEDIO));
+        veiculos.add(new Veiculo("Volkswagen", "Jetta", "123456789", Util.Tamanho.SUV));
+        return veiculos;
+    }
+    public static List<Cliente> loadSampleClientes(){
+        List<Cliente> clientes = new ArrayList<>();
+        clientes.add(new ClientePF("Caio Brito", "123.456.789-10"));
+        clientes.add(new ClientePF("Ingrid Gomes", "321.654.987-11"));
+        clientes.add(new ClientePJ("COCA COLA", "45.997.418/0017-10"));
+        clientes.add(new ClientePJ("PEPSICO DO BRASIL", "31.565.104/0020-30"));
+        return clientes;
+    }
+
+    public static void main(String[] args) {
+    SistemaClientes sistemaClientes = new SistemaClientes(loadSampleClientes());
+    SistemaVeiculos sistemaVeiculos = new SistemaVeiculos(loadSampleVeiculos());
+
+    SistemaAluguel sistemaDeAluguel = new SistemaAluguel(sistemaVeiculos,sistemaClientes);
+
+    ControllerLocadora controllerLocadora = new ControllerLocadora("Locadora Dois Irmãos",sistemaDeAluguel);
+
+    App cliApp = new App(controllerLocadora);
+
+    cliApp.run();
+    }
+}
