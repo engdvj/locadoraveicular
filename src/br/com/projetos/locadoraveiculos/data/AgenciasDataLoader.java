3be115934e4a09ac0765637764214c96294f0197
engdvj <eng.dvj@gmail.com>
2024-03-17 21:42:05 -0300

    ContratosDataLoader

--- src/br/com/projetos/locadoraveiculos/data/AgenciasDataLoader.java
+++ src/br/com/projetos/locadoraveiculos/data/AgenciasDataLoader.java
@@ -1,16 +1,20 @@
 package br.com.projetos.locadoraveiculos.data;
 
 import br.com.projetos.locadoraveiculos.model.entidades.agencia.Agencia;
-
 import java.time.LocalTime;
 
-import static br.com.projetos.locadoraveiculos.data.ContratosDataLoader.*;
-
 public class AgenciasDataLoader {
-    public static Agencia loadSampleAgencia(){
+    public static Agencia loadSampleAgencia() {
         String nome = "Dois Irmãos";
         String endereco = "Rua Duque de Caxias";
-        Agencia agencia = new Agencia(nome, endereco, LocalTime.of(9, 0), LocalTime.of(18, 0),loadSampleContratosAtivos(endereco),loadSampleContratosInativos(loadSampleContratosAtivos(endereco)) );
+        LocalTime horarioAbertura = LocalTime.of(9, 0);
+        LocalTime horarioFechamento = LocalTime.of(18, 0);
+
+        ContratosDataLoader contratosDataLoader = new ContratosDataLoader();
+        ContratosDataLoader.ResultadoContratos resultado = contratosDataLoader.processarContratos(endereco);
+
+        Agencia agencia = new Agencia(nome, endereco, horarioAbertura, horarioFechamento, resultado.getContratosAtivos(), resultado.getContratosInativos());
+
         return agencia;
     }
 }
--- src/br/com/projetos/locadoraveiculos/data/ContratosDataLoader.java
+++ src/br/com/projetos/locadoraveiculos/data/ContratosDataLoader.java
@@ -1,118 +1,91 @@
 package br.com.projetos.locadoraveiculos.data;
 
-import br.com.projetos.locadoraveiculos.model.entidades.agencia.Agencia;
 import br.com.projetos.locadoraveiculos.model.entidades.clientes.Cliente;
 import br.com.projetos.locadoraveiculos.model.entidades.veiculo.Veiculo;
 import br.com.projetos.locadoraveiculos.model.eventos.Aluguel;
 import br.com.projetos.locadoraveiculos.model.eventos.Devolucao;
 
-import java.time.*;
+import java.time.LocalDateTime;
+import java.time.Month;
 import java.util.*;
 
-
-import static br.com.projetos.locadoraveiculos.data.AgenciasDataLoader.loadSampleAgencia;
-import static br.com.projetos.locadoraveiculos.data.ClientesDataLoader.*;
-import static br.com.projetos.locadoraveiculos.data.VeiculosDataLoader.*;
+import static br.com.projetos.locadoraveiculos.data.ClientesDataLoader.loadSampleClientes;
+import static br.com.projetos.locadoraveiculos.data.VeiculosDataLoader.loadSampleVeiculos;
 
 public class ContratosDataLoader {
 
-    public static HashSet<Aluguel> loadSampleContratosAtivos(String endereco) {
+    public ResultadoContratos processarContratos(String endereco) {
+        HashSet<Aluguel> contratosAtivos = criarContratosAtivos(endereco);
+        HashSet<Devolucao> contratosInativos = inativarContratos(contratosAtivos);
+        imprimirContratos(contratosAtivos, contratosInativos);
 
+        return new ResultadoContratos(contratosAtivos, contratosInativos);
+    }
+
+    private HashSet<Aluguel> criarContratosAtivos(String endereco) {
         HashSet<Veiculo> veiculos = loadSampleVeiculos();
         HashSet<Cliente> clientes = loadSampleClientes();
 
         HashSet<Aluguel> contratosAtivos = new HashSet<>();
-
         Iterator<Veiculo> veiculoIterator = veiculos.iterator();
         Iterator<Cliente> clienteIterator = clientes.iterator();
 
         int index = 0;
-        while (veiculoIterator.hasNext() && clienteIterator.hasNext() && index < veiculos.size() && index < clientes.size()) {
+        while (veiculoIterator.hasNext() && clienteIterator.hasNext()) {
             LocalDateTime dataEvento = LocalDateTime.of(2024, Month.MARCH, 10 + index, 10 + (index % 4), 0);
-            Aluguel aluguel = new Aluguel(veiculoIterator.next(), clienteIterator.next(), dataEvento,endereco);
+            Aluguel aluguel = new Aluguel(veiculoIterator.next(), clienteIterator.next(), dataEvento, endereco);
             contratosAtivos.add(aluguel);
             index++;
         }
 
-        // Opcional: Imprime os contratos para verificação
+        return contratosAtivos;
+    }
+
+    private HashSet<Devolucao> inativarContratos(HashSet<Aluguel> contratosAtivos) {
+        HashSet<Devolucao> contratosInativos = new HashSet<>();
+        List<Aluguel> contratosList = new ArrayList<>(contratosAtivos);
+        Collections.shuffle(contratosList);
+
+        for (int i = 0; i < contratosList.size() && contratosInativos.size() < 3; i++) {
+            Aluguel aluguel = contratosList.get(i);
+            if (i % 2 == 0) {
+                LocalDateTime dataDevolucao = aluguel.dataRetirada().plusDays(5);
+                Devolucao devolucao = new Devolucao(aluguel, dataDevolucao);
+                contratosInativos.add(devolucao);
+                contratosAtivos.remove(aluguel);
+            }
+        }
+
+        return contratosInativos;
+    }
+
+    private void imprimirContratos(HashSet<Aluguel> contratosAtivos, HashSet<Devolucao> contratosInativos) {
         System.out.println("---------------Contratos Ativos---------------");
         for (Aluguel aluguel : contratosAtivos) {
             System.out.println(aluguel);
         }
 
-        // Assumindo que você tem um método para adicionar os contratos ativos ao sistema
-        // Adicione os contratos ativos ao sistema aqui
-        return contratosAtivos;
-    }
-
-    public static HashSet<Devolucao> loadSampleContratosInativos(HashSet<Aluguel> contratosAtivos) {
-        HashSet<Devolucao> contratosInativos = new HashSet<>();
-
-        List<Aluguel> contratosList = new ArrayList<>(contratosAtivos);
-        Collections.shuffle(contratosList); // Mistura os contratos para a seleção aleatória
-
-        for (int index = 0; index < contratosList.size() && contratosInativos.size() < 3; index++) {
-            Aluguel aluguel = contratosList.get(index);
-            if (index % 2 == 0) {
-                LocalDateTime dataDevolucao = aluguel.dataRetirada().plusDays(5); // Simula a devolução 5 dias após o aluguel
-                Devolucao devolucao = new Devolucao(aluguel, dataDevolucao);
-                contratosInativos.add(devolucao);
-            }
-        }
-
-        // Remove os contratos inativados da lista de ativos
         System.out.println("---------------Contratos Inativos---------------");
         for (Devolucao devolucao : contratosInativos) {
             System.out.println(devolucao);
-            contratosAtivos.remove(devolucao.aluguel());
+        }
+    }
+
+    public static class ResultadoContratos {
+        private HashSet<Aluguel> contratosAtivos;
+        private HashSet<Devolucao> contratosInativos;
+
+        public ResultadoContratos(HashSet<Aluguel> contratosAtivos, HashSet<Devolucao> contratosInativos) {
+            this.contratosAtivos = contratosAtivos;
+            this.contratosInativos = contratosInativos;
         }
 
-        return contratosInativos;
+        public HashSet<Aluguel> getContratosAtivos() {
+            return contratosAtivos;
+        }
+
+        public HashSet<Devolucao> getContratosInativos() {
+            return contratosInativos;
+        }
     }
 }
-
-
-
-    //    public static HashSet<Aluguel> loadSampleContratos() {
-//        HashSet<Veiculo> veiculos = loadSampleVeiculos();
-//        HashSet<Cliente> clientes = loadSampleClientes();
-//
-//        HashSet<Aluguel> contratosAtivos = new HashSet<>();
-//        HashSet<Devolucao> contratosInativos = new HashSet<>();
-//
-//        Iterator<Veiculo> veiculoIterator = veiculos.iterator();
-//        Iterator<Cliente> clienteIterator = clientes.iterator();
-//
-//        int index = 0;
-//        while (veiculoIterator.hasNext() && clienteIterator.hasNext() && index < veiculos.size() && index < clientes.size()) {
-//            LocalDateTime dataEvento = LocalDateTime.of(2024, Month.MARCH, 10 + index, 10 + (index % 4), 0);
-//            Aluguel aluguel = new Aluguel(veiculoIterator.next(), clienteIterator.next(), dataEvento);
-//            contratosAtivos.add(aluguel);
-//
-//            // Decide aleatoriamente quais contratos serão marcados como inativos
-//            if (index % 2 == 0 && contratosInativos.size() < 3) { // Exemplo: marca cada segundo contrato como inativo até ter 3
-//                LocalDateTime dataDevolucao = dataEvento.plusDays(5); // Simula a devolução 5 dias após o aluguel
-//                Devolucao devolucao = new Devolucao(aluguel, dataDevolucao);
-//                contratosInativos.add(devolucao);
-//                contratosAtivos.remove(aluguel);
-//            }
-//
-//            index++;
-//        }
-//
-//        // Opcional: Imprime os contratos para verificação
-//        System.out.println("---------------Contratos Ativos---------------");
-//        for (Aluguel aluguel : contratosAtivos) {
-//            System.out.println(aluguel);
-//        }
-//
-//        System.out.println("---------------Contratos Inativos---------------");
-//        for (Devolucao devolucao : contratosInativos) {
-//            System.out.println(devolucao);
-//        }
-//
-//        // Assumindo que você tem métodos para adicionar esses contratos ao sistema
-//        // Adicione os contratos ativos e inativos ao sistema aqui
-//        return contratosAtivos;
-//    }
-
